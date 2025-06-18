54fc97f154970d73b3c32298ebe54509c522af13
ahn-h-j <tkgkd246@gmail.com>
2025-06-18 22:43:07 +0900

    MethodChaining

---
+++ src/lang/string/chaining/MethodChainingMain2.java
@@ -0,0 +1,18 @@
+package lang.string.chaining;
+
+public class MethodChainingMain2 {
+    public static void main(String[] args) {
+        ValueAdder adder = new ValueAdder();
+        ValueAdder adder1 = adder.add(1);
+        ValueAdder adder2 = adder1.add(2);
+        ValueAdder adder3 = adder2.add(3);
+
+        int result = adder3.getValue();
+        System.out.println("result = " + result);
+
+        ValueAdder valueAdder = new ValueAdder();
+        int valueResult = valueAdder.add(1).add(2).add(3).getValue();
+
+        System.out.println("valueResult = " + valueResult);
+    }
+}
