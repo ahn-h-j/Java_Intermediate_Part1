f321d4060937a6f8bb7646d760bab518c91e5aae
ahn-h-j <tkgkd246@gmail.com>
2025-06-30 09:59:59 +0900

    Exception 예제 : 네트워크 프로그램

---
+++ src/exception/ex0/NetworkServiceV0.java
@@ -0,0 +1,13 @@
+package exception.ex0;
+
+public class NetworkServiceV0 {
+    public void sendMessage(String data){
+        String address = "http://example.com";
+        NetworkClientV0 networkClientV0 = new NetworkClientV0(address);
+
+
+        networkClientV0.connect();
+        networkClientV0.send(data);
+        networkClientV0.disconnect();
+    }
+}
